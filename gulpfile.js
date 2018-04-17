ba8e14e57d49a2390daa22cb4032472ff54ba9cc
Olga Grigorash <rizraelleshinobu@gmail.com>
2018-04-17 17:27:50 +0300

    Add new changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,11 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var nested = require('postcss-nested');
-var clean = require('gulp-clean-css');
 
 gulp.task('css', function () {
     var plugins = [
       nested(),
       autoprefixer(),
-      clean(),
     ];
     return gulp.src('css/main.css')
         .pipe(postcss(plugins))
