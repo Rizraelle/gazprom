a7819ff1bd9c5266a9dfb7fc7f89c48c7008af03
Olga Grigorash <rizraelleshinobu@gmail.com>
2018-04-18 17:05:56 +0300

    Add new changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Main slider
+
+// $(document).ready(function(){
+//     $('.main__slick').slick({
+//         slidesToShow: 3,
+//         infinite: false,
+//         slidesToScroll: 1,
+//         dots:false,
+//         adaptiveHeight:true,
+//         pauseOnHover:false
+//     });
+// });
