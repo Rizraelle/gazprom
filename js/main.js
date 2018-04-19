71968ac6d80d3b07f107d0916b96b2ffa3467f85
Olga Grigorash <rizraelleshinobu@gmail.com>
2018-04-19 10:38:38 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 //Main slider
 
-// $(document).ready(function(){
-//     $('.main__slick').slick({
-//         slidesToShow: 3,
-//         infinite: false,
-//         slidesToScroll: 1,
-//         dots:false,
-//         adaptiveHeight:true,
-//         pauseOnHover:false
-//     });
-// });
+$(document).ready(function(){
+    $('.main__slick').slick({
+        slidesToShow: 3,
+        infinite: false,
+        slidesToScroll: 1,
+        dots:false,
+        adaptiveHeight:true,
+        pauseOnHover:false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
