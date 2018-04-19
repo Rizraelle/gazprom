5bb64143648b13d6a6fa0345f484ae51718545f4
Olga Grigorash <rizraelleshinobu@gmail.com>
2018-04-19 15:33:06 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,65 +1,70 @@
-// function newClass(){
-//     var block = document.querySelector(".menu__line--list");
-//     if (window.matchMedia('(max-width: 1200px)').matches) {
-//         block.classList.add('menu__slider');
-//     } else {
-//         block.classList.remove('slider');
-//     }
-// };
-//
-// newClass();
-// $(window).resize(newClass);
-//
-//
-// //Main slider
-//
-// $(document).ready(function(){
-//     $('.main__slick').slick({
-//         slidesToShow: 3,
-//         infinite: false,
-//         slidesToScroll: 1,
-//         dots:false,
-//         adaptiveHeight:true,
-//         pauseOnHover:false,
-//         responsive: [
-//             {
-//                 breakpoint: 1200,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1,
-//                     infinite: true
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             }
-//         ]
-//     });
-//     $('.menu__slider').slick({
-//         slidesToShow: 8,
-//         infinite: false,
-//         dots:false,
-//         adaptiveHeight:true,
-//         pauseOnHover:false,
-//         responsive: [
-//             {
-//                 breakpoint: 1200,
-//                 settings: {
-//                     slidesToShow: 6,
-//                     slidesToScroll: 1
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             }
-//         ]
-//     });
-// });
+function newClass(){
+    var block = document.querySelector(".menu__line--list");
+    if (window.matchMedia('(max-width: 1200px)').matches) {
+        block.classList.add('menu__slider');
+    } else {
+        block.classList.remove('slider');
+    }
+};
+
+newClass();
+$(window).resize(newClass);
+
+
+//Main slider
+
+$(document).ready(function(){
+    $('.main__slick').slick({
+        slidesToShow: 3,
+        infinite: false,
+        slidesToScroll: 1,
+        dots:false,
+        adaptiveHeight:true,
+        pauseOnHover:false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.menu__slider').slick({
+        slidesToShow: 8,
+        infinite: false,
+        dots:false,
+        adaptiveHeight:true,
+        pauseOnHover:false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+
+document.getElementById('search__button').onclick = function(){
+    document.getElementsByClassName('search_input')[0].classList.toggle("show__input");
+}
