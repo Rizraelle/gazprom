6ff370736516e4f6c9fe2f25afc8488b7a1dc672
Olga Grigorash <rizraelleshinobu@gmail.com>
2018-04-20 16:50:28 +0300

    Changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Trying to add slider for header
+
 function newClass(){
     var block = document.querySelector(".menu__line--list");
     if (window.matchMedia('(max-width: 1200px)').matches) {
@@ -13,60 +15,76 @@ $(window).resize(newClass);
 
 //Main slider
 
-$(document).ready(function(){
-    $('.main__slick').slick({
-        slidesToShow: 3,
-        infinite: false,
-        slidesToScroll: 1,
-        dots:false,
-        adaptiveHeight:true,
-        pauseOnHover:false,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    arrows: false
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false
-                }
-            }
-        ]
-    });
-    // $('.menu__slider').slick({
-    //     slidesToShow: 8,
-    //     infinite: false,
-    //     dots:false,
-    //     adaptiveHeight:true,
-    //     pauseOnHover:false,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1200,
-    //             settings: {
-    //                 slidesToShow: 6,
-    //                 slidesToScroll: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ]
-    // });
-});
+// $(document).ready(function(){
+//     $('.main__slick').slick({
+//         slidesToShow: 3,
+//         infinite: false,
+//         slidesToScroll: 1,
+//         dots:false,
+//         draggable:false,
+//         adaptiveHeight:true,
+//         pauseOnHover:false,
+//         responsive: [
+//             {
+//                 breakpoint: 1200,
+//                 settings: {
+//                     slidesToShow: 2,
+//                     slidesToScroll: 1,
+//                     infinite: true,
+//                     arrows: false,
+//                     dots: true,
+//                     appendDots:$('#dots'),
+//                     draggable:true
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     arrows: false,
+//                     dots: true,
+//                     draggable:true
+//                 }
+//             }
+//         ]
+//     });
+//     $('.menu__slider').slick({
+//         slidesToShow: 8,
+//         infinite: false,
+//         dots:false,
+//         adaptiveHeight:true,
+//         pauseOnHover:false,
+//         responsive: [
+//             {
+//                 breakpoint: 1200,
+//                 settings: {
+//                     slidesToShow: 6,
+//                     slidesToScroll: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
+// });
 
+
+//Smooth search input
 
 document.getElementById('search__button').onclick = function(){
     document.getElementsByClassName('search_input')[0].classList.toggle("show__input");
-}
+};
+
+
+
+//Remove focus from inputs etc.
+
+$("button").mouseup(function(){
+    $(this).blur();
+})
