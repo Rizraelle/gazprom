54efe89e91a6a1fe877f8abdea1be33f3768acd7
R45H <gmr45h@gmail.com>
2018-04-24 11:45:57 +0300

    Add swiper slider

--- js/main.js
+++ js/main.js
@@ -15,64 +15,28 @@ $(window).resize(newClass);
 
 //Main slider
 
-// $(document).ready(function(){
-//     $('.main__slick').slick({
-//         slidesToShow: 3,
-//         infinite: false,
-//         slidesToScroll: 1,
-//         dots:false,
-//         draggable:false,
-//         adaptiveHeight:true,
-//         pauseOnHover:false,
-//         responsive: [
-//             {
-//                 breakpoint: 1200,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1,
-//                     infinite: true,
-//                     arrows: false,
-//                     dots: true,
-//                     appendDots:$('#dots'),
-//                     draggable:true
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1,
-//                     arrows: false,
-//                     dots: true,
-//                     draggable:true
-//                 }
-//             }
-//         ]
-//     });
-//     $('.menu__slider').slick({
-//         slidesToShow: 8,
-//         infinite: false,
-//         dots:false,
-//         adaptiveHeight:true,
-//         pauseOnHover:false,
-//         responsive: [
-//             {
-//                 breakpoint: 1200,
-//                 settings: {
-//                     slidesToShow: 6,
-//                     slidesToScroll: 1
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             }
-//         ]
-//     });
-// });
+$(function() {
+
+	var mySwiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+		breakpoints: {
+			991: {
+				slidesPerView: 2
+			},
+			767: {
+				slidesPerView: 1
+			}
+		}
+   });
+});
 
 
 //Smooth search input
